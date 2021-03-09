be8a6c6d0243c2460e8ad2b0369611f7a8ce9a26
Волков Вячеслав <58400506+volkovVA@users.noreply.github.com>
2021-03-09 21:23:47 +0300

    added new fixes

--- src/blocks/components/pack-block/pack-block.js
+++ src/blocks/components/pack-block/pack-block.js
@@ -1,27 +1,27 @@
 const selectPack = () => {
-    const packBlock = Array.prototype.slice.apply(document.querySelectorAll('.js-pack-block'));
+    const packBlock = Array.prototype.slice.apply(document.querySelectorAll(".js-pack-block"));
 
     packBlock.forEach(el => {
-        el.addEventListener('click', (event) => {
-            const pack = el.querySelector('.js-pack');
+        el.addEventListener("click", (event) => {
+            const pack = el.querySelector(".js-pack");
             const target = event.target,
-                  packTarget = target.closest('.js-pack'),
-                  buyTarget = target.closest('.js-buy');
+                packTarget = target.closest(".js-pack"),
+                buyTarget = target.closest(".js-buy");
 
             if (packTarget || buyTarget) {
-                el.classList.toggle('pack-block--selected');
-                pack.classList.add('pack--default');
-                el.addEventListener('mouseleave', () => {
-                    pack.classList.remove('pack--default');
+                el.classList.toggle("pack-block--selected");
+                pack.classList.add("pack--default");
+                el.addEventListener("mouseleave", () => {
+                    pack.classList.remove("pack--default");
                 });
             }
 
-            if (el.classList.contains('pack-block--disabled')) {
-                el.classList.remove('pack-block--selected');
-                pack.classList.remove('pack--default');
+            if (el.classList.contains("pack-block--disabled")) {
+                el.classList.remove("pack-block--selected");
+                pack.classList.remove("pack--default");
             }
         });
     });
-}
+};
 
 selectPack();
