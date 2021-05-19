466ad83f4f23669538c480d236e782041236a89b
volkovVA <volkov.slava@hotmail.com>
2021-05-20 00:55:01 +0300

    feat: add accordion js-code

---
+++ src/js/import/components.js
@@ -0,0 +1,25 @@
+window.onload = function () {
+  activateAcc();
+};
+
+const activateAcc = () => {
+  const acc = document.querySelector(".faq__acc");
+
+  acc.addEventListener('click', (event) => {
+    const target = event.target;
+    const parent = target.parentElement;
+    const accItem = document.querySelectorAll(".faq__acc-item");
+
+    if (target.classList.contains('faq__question')) {
+      if (parent.classList.contains('active')) {
+        parent.classList.remove('active');
+        return;
+      }
+      accItem.forEach(el => {
+        el.classList.remove('active');
+      });
+      parent.classList.add('active');
+    }
+  });
+};
+
