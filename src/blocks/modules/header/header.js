1fe01cc68beeaa4caca0918aa288935ae9040c50
volkovVA <volkov.slava@hotmail.com>
2021-06-04 16:29:08 +0300

    feat: add certificate slider

---
+++ src/blocks/modules/header/header.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
